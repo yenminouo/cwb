20706754a4a18dc15f6d15da26460a08e0f784a4
yenminouo <yenminchen.owo@gmail.com>
2017-12-26 20:21:48 +0800

    Signed-off-by: yenminouo <yenminchen.owo@gmail.com>

--- w3.R
+++ w3.R
@@ -1,5 +1,8 @@
 number = c(1:13,1:13,1:13,1:13)
+number1 = rep(1:13,4)
+identical(number,number1)
 suit = c(rep("黑桃",13),rep("紅心",13),rep("方塊",13),rep("梅花",13))
 cards = paste(suit,number)
 sample(cards,5)
+str(cards)
 
