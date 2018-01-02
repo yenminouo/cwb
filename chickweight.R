ca26357fd4c731404ecd64c926b887b2eae44894
yenminouo <yenminchen.owo@gmail.com>
2018-01-02 20:25:05 +0800

    Signed-off-by: yenminouo <yenminchen.owo@gmail.com>

--- chickweight.R
+++ chickweight.R
@@ -5,10 +5,12 @@ y=ChickWeight$weight
 identical(x,y)
 z=ChickWeight[,c(1,4)]
 ChickWeight[,c("weight","Diet")]
-m=ChickWeight[rc(1,3,5,7,9),]
+m=ChickWeight[c(1,3,5,7,9),]
 nrow(c)
-n=c[c$weight==48]
+n=c[c$weight==48,]
 plot(n$weight)
 o=subset(ChickWeight,Chick==48,select=c(Time,weight))
 identical(n,o)
 plot(o$weight)
+boxplot(ChickWeight$weight ~ ChickWeight$Diet, las=1)
+
